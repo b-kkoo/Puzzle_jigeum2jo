7d7d2b33e2d5ccb096c2c1890452d3fffa70a49e
Inhye Lee <121735547+eveinhyelee@users.noreply.github.com>
2024-11-19 20:54:27 +0900

    [Feature] UIManager 프리팹 추가

--- Assets/02.Scripts/Managers/GameManager.cs
+++ Assets/02.Scripts/Managers/GameManager.cs
@@ -6,19 +6,20 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
-    
+
     private UIManager uiManager;
     public UIManager UIManager { get; set; }
-    
+
     private SoundManager soundManager;
     public SoundManager SoundManager { get; set; }
 
-    //[SerializeField] private GameObject uIManagerPrefab;
+    [SerializeField] private GameObject uIManagerPrefab;
     [SerializeField] private GameObject soundManagerPrefab;
+
     void Awake()
     {
         // 싱글톤 유지: 기존 인스턴스가 없으면 생성해주고, 스크립트를 추가해준다.
-        if (instance == null )
+        if (instance == null)
         {
             instance = this;
             DontDestroyOnLoad(gameObject); // 씬 전환 시 삭제되지 않도록 설정
@@ -29,13 +30,13 @@ public class GameManager : MonoBehaviour
             return;
         }
 
-        //uiManager = GetComponentInChildren<UIManager>();
-        // if (uiManager == null)
-        // {
-        //     GameObject uiManagerObject = Instantiate(uIManagerPrefab);
-        //     uiManagerObject.transform.SetParent(transform); // GameManager의 자식으로 설정
-        //     uiManager = uiManagerObject.GetComponent<UIManager>();
-        // }
+        uiManager = GetComponentInChildren<UIManager>();
+        if (uiManager == null)
+        {
+            GameObject uiManagerObject = Instantiate(uIManagerPrefab);
+            uiManagerObject.transform.SetParent(transform); // GameManager의 자식으로 설정
+            uiManager = uiManagerObject.GetComponent<UIManager>();
+        }
 
         soundManager = GetComponentInChildren<SoundManager>();
         if (soundManager == null)
