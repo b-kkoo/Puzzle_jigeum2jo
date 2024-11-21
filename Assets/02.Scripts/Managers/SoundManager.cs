9d2e7dbe86d6f97355d4a55865725d58e5e801a3
Inhye Lee <121735547+eveinhyelee@users.noreply.github.com>
2024-11-21 11:52:22 +0900

    [Feature] 사운드매니져스크립트
    프리팹변경

--- Assets/02.Scripts/Managers/SoundManager.cs
+++ Assets/02.Scripts/Managers/SoundManager.cs
@@ -2,16 +2,22 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
-    public static SoundManager instance;
     [SerializeField][Range(0f, 1f)] private float soundEffectVolume;
-    [SerializeField][Range(0f, 1f)] private float soundEffectPitchVariance;
     [SerializeField][Range(0f, 1f)] private float musicVolume;
-
+    [SerializeField][Range(0f, 1f)] private float effectVolume;
+    
+    public Slider backGroundMusicSlider;
+    public Slider effectVoulemSlider;
+    
     public AudioSource audioSource;
     public AudioClip mainMusicClip;
+    public AudioClip InGameMusicClip;
     
     
     void Awake()
@@ -20,12 +26,23 @@ public class SoundManager : MonoBehaviour
         
         audioSource = GetComponent<AudioSource>();
         audioSource.volume = musicVolume;
+        /*backGroundMusicSlider.value = audioSource.volume;
+        audioSource.volume = backGroundMusicSlider.value;*/
     }
 
     private void Start()
     {
         PlayMusic(mainMusicClip);
     }
+
+    /*private void Update()
+    {
+        if (SceneManager.GetActiveScene().buildIndex == 1) // 게임씬일때
+        {
+            ChangeBackGroundMusic(InGameMusicClip);
+        }
+
+    }*/
 
     public void PlayMusic(AudioClip music)
     {
