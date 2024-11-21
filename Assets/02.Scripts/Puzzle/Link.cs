e00273909af07e61a33fa341f3d20a3756549cfa
b-kkoo <bmcow331@naver.com>
2024-11-21 10:50:46 +0900

    [test] 회전 손잡이

--- Assets/02.Scripts/Puzzle/Link.cs
+++ Assets/02.Scripts/Puzzle/Link.cs
@@ -4,41 +4,5 @@ using UnityEngine;
 
 public class Link : MonoBehaviour
 {
-    [SerializeField] private GameObject bridge;
-    [SerializeField] private int linkedAngle;
-
-    public List<GameObject> linkedObject = new List<GameObject>();
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        for(int i = 0; i < linkedObject.Count; i++)
-        {
-            linkedObject[i].SetActive(false);
-        }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        CheckBridgeAngle();
-    }
-
-    void CheckBridgeAngle()
-    {
-        if (bridge.transform.rotation.x == linkedAngle)
-        {
-            for (int i = 0; i < linkedObject.Count; i++)
-            {
-                linkedObject[i].SetActive(true);
-            }
-        }
-        else
-        {
-            for (int i = 0; i < linkedObject.Count; i++)
-            {
-                linkedObject[i].SetActive(false);
-            }
-        }
-    }
+    //bridge의 각도에 따라 path를 끊거나 연결
 }
