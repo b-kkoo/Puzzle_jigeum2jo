66c1367bc1e343e4f1971ece5ae04c5657c8bebf
Inhye Lee <121735547+eveinhyelee@users.noreply.github.com>
2024-11-21 21:37:06 +0900

    [Fixed] SoundSlider 동기화

--- Assets/02.Scripts/UISoundController.cs
+++ Assets/02.Scripts/UISoundController.cs
@@ -8,19 +8,25 @@ public class UISoundController : UIBase
 {
     [SerializeField]private Slider backGroundMusicSlider;
     [SerializeField]private Slider effectVolumeSlider;
-
+    private SoundManager sound;
+    
     private void Start()
     {
+        sound = GameManager.instance.SoundManager;
+        
+        backGroundMusicSlider.value = sound.audioSource.volume;
+        effectVolumeSlider.value = sound.GameAudioSource.volume;
+        
         backGroundMusicSlider.onValueChanged.AddListener(ChangeVolume);
         effectVolumeSlider.onValueChanged.AddListener(ChangeEffectVolume);
     }
 
     public void ChangeVolume(float volume)
     {
-        GameManager.instance.SoundManager.ChangeVolume(volume);
+        sound.ChangeVolume(volume);
     }
     public void ChangeEffectVolume(float volume)
     {
-        GameManager.instance.SoundManager.ChangeEffectVolume(volume);
+        sound.ChangeEffectVolume(volume);
     }
 }
